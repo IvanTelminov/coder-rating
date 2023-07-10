e0133cc9baac3d9b4385f52f5d84b4b51192f30c
ivan <ivan@telminov.ru>
2023-07-10 20:12:34 +0300

    Добавил меню

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,20 @@
 </script>
 
 <template>
+
+  <el-menu     mode="horizontal" :ellipsis="false">
+    <el-menu-item>Рейтинг</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">
+      <a href="http://127.0.0.1:8000/backend/admin/">Админка</a>
+    </el-menu-item>
+  </el-menu>
+
   <router-view></router-view>
 </template>
 
 <style scoped>
-
+.flex-grow {
+  flex-grow: 1;
+}
 </style>
