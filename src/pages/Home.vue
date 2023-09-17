980c1ee567d57c3b44f858ffa399d0b6884575fb
ivan <ivan@telminov.ru>
2023-09-17 15:46:39 +0300

    #5 Вывел кодеров в виде таблицы

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,27 @@
 <script setup>
+  import {useCoderStore} from "../stores/coder.js";
 
+  const coder = useCoderStore()
+  coder.load()
 </script>
 
 <template>
   <h2>Home</h2>
+
+
+  <el-table :data="coder.list">
+    <el-table-column prop="name" label="Имя"/>
+    <el-table-column prop="score" label="Счёт"/>
+    <el-table-column label="Темы">
+      <template #default="scope">
+        <span
+            v-for="u in scope.row.units" :key="u.id">
+          {{u.name}}
+        </span>
+      </template>
+    </el-table-column>
+  </el-table>
+
 </template>
 
 <style scoped>
